2af0bfb04920fb17d0a7edded3646dd162e1725e
Harkai Iosif <harkai222@gmail.com>
2022-10-17 17:03:18 +0300

    Add project files.

---
+++ Nr maxim dintr-o serie Collatz/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nr_maxim_dintr_o_serie_Collatz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = -1;
+            int n = int.Parse(Console.ReadLine());
+            while (n != 1)
+            {
+                if (n > max) max = n;
+                if (n % 2 == 0) n = n / 2;
+                else n = 3 * n + 1;
+            }
+            Console.WriteLine(max);
+            Console.ReadKey();
+        }
+    }
+}
